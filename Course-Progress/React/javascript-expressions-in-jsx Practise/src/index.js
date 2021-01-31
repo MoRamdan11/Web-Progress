7eae03a427f736ff523f7358e1242b66205c4f36
Mohamed Ramdan <64365635+MoRamdan11@users.noreply.github.com>
2021-01-31 02:35:54 +0200

    Update index.js

--- Course-Progress/React/javascript-expressions-in-jsx Practise/src/index.js
+++ Course-Progress/React/javascript-expressions-in-jsx Practise/src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-var fName = "Angela";
-var lName = "Yu";
-var randNumb = Math.floor(Math.random() * 100 + 1); //1 --> 100
+//Create a react app from scratch.
+var name = "Mohamed Ramdan";
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+
 ReactDOM.render(
   <div>
-    <h1>Hello {fName + " " + lName}!</h1>
-    <h1>{`${fName} ${lName}`}</h1>
-    <p>Your lucky number is {randNumb}</p>
+    <p>Created by {name}</p>
+    <p>All rights Reserved &copy; {currentYear}</p>
   </div>,
-  document.getElementById("root")
+  document.querySelector("#root")
 );
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
